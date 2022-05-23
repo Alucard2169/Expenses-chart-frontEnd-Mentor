d8b0813568934500e2c9d9f41b60c07bdcdb9010
Alucard2169 <ashish10deepanshu@gmail.com>
2022-05-23 13:31:23 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const days = document.querySelectorAll('.ch');
+const total = document.getElementById('sp');
+
+// total sum
+let sum = 0;
+
+
+// check current day and set the color accordingly
+const date = new Date().getDay();
+days.forEach(day => {
+    if (parseInt(day.dataset.day) === date) {
+            day.style.backgroundColor = 'hsl(186, 34%, 60%)'
+    }
+    else {
+        day.style.backgroundColor = `hsl(10, 79%, 65%)`
+    }
+    
+})
+
+// get data from .json file
+const data = async () => {
+    const res = await fetch('./data.json');
+    const Data = await res.json();
+
+    for (let i = 0; i < Data.length; i++){
+        sum += Data[i].amount;
+        days[i].style.height = `${Data[i].amount}%`
+        days[i].setAttribute('value', `$${Data[i].amount}`);
+    }
+    total.textContent = sum;
+}
+
+data()
+
+
